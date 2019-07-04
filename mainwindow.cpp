633462658a5d800c083147cf2834007b850ea422
Vartul Tripathi <vartult2@gmail.com>
2019-07-04 08:33:45 +0530

    Enhancement

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,7 @@ int hardcord[5][8]={
                     {16,19,26,31,4,4,6,39},
                     {14,16,23,26,18,21,17,14}
                    };
-
+int digits[5][13]={0};
 int max_num=INT_MIN;
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -68,9 +68,11 @@ void MainWindow::on_Button_released()
         count++;
         QString line = in.readLine();
         fields = line.split(",");
+        int hi=1,hj=2;
+        int deci=0;
         for(int i=0;i<fields.size();i++)
         {
-            int deci=0;
+
             if(fields[i][0]=='0')
                 deci+=0*16;
             else if(fields[i][0]=='1')
@@ -136,168 +138,161 @@ void MainWindow::on_Button_released()
                 deci+=14;
             else
                 deci+=15;
-            qDebug() << "Amount" << fields[i]   <<" "<<deci;
+            qDebug() << "Amount" << fields[i]<<" "<<deci;
+
+            digits[hi][hj]=deci;
+            hj++;
+            deci=0;
+            if(hj==9)
+            {
+                hi++;
+                hj=2;
+            }
+            if(hi==6)
+                  break;
 
         }
     }
 
 
-
-//    for(int i=0;fields.size();i++)
-//    {
-//        QMessageBox::warning(this,"data",fields[i]);
-//    }
-
-
-
-
-//        while (!file.atEnd()) {
-//            lines= file.readLine();
-//            lines.split(',');
-//            for(int i=0;i<lines.count();i++)
-//            {
-//                QMessageBox::warning(this,"data",qUtf8Printable(lines));
-//                //QMessageBox::warning(this,"data",QString(c));
-//                //ui->Result->display();
-//            }
 if(y=='1'||y=='o'){
-    ui->lcd00->display(hardcord[1][0]);
-    ui->lcd01->display(hardcord[1][1]);
-    ui->lcd03->display(hardcord[1][2]);
-    ui->lcd04->display(hardcord[1][3]);
-    ui->lcd05->display(hardcord[1][4]);
-    ui->lcd06->display(hardcord[1][5]);
-    ui->lcd07->display(hardcord[1][6]);
-    ui->lcd08->display(hardcord[1][7]);
-    ui->lcd10->display(hardcord[2][0]);
-    ui->lcd11->display(hardcord[2][1]);
-    ui->lcd12->display(hardcord[2][2]);
-    ui->lcd14->display(hardcord[2][3]);
-    ui->lcd15->display(hardcord[2][4]);
-    ui->lcd16->display(hardcord[2][5]);
-    ui->lcd17->display(hardcord[2][6]);
-    ui->lcd18->display(hardcord[2][7]);
-    ui->lcd21->display(hardcord[3][0]);
-    ui->lcd22->display(hardcord[3][1]);
-    ui->lcd23->display(hardcord[3][2]);
-    ui->lcd24->display(hardcord[3][3]);
-    ui->lcd25->display(hardcord[3][4]);
-    ui->lcd26->display(hardcord[3][5]);
-    ui->lcd27->display(hardcord[3][6]);
-    ui->lcd28->display(hardcord[3][7]);
-    ui->lcd31->display(hardcord[4][0]);
-    ui->lcd32->display(hardcord[4][1]);
-    ui->lcd33->display(hardcord[4][2]);
-    ui->lcd34->display(hardcord[4][3]);
-    ui->lcd35->display(hardcord[4][4]);
-    ui->lcd36->display(hardcord[4][5]);
-    ui->lcd37->display(hardcord[4][6]);
-    ui->lcd38->display(hardcord[4][7]);
+    ui->lcd00->display(digits[1][0+2]);
+    ui->lcd01->display(digits[1][1+2]);
+    ui->lcd03->display(digits[1][2+2]);
+    ui->lcd04->display(digits[1][3+2]);
+    ui->lcd05->display(digits[1][4+2]);
+    ui->lcd06->display(digits[1][5+2]);
+    ui->lcd07->display(digits[1][6+2]);
+    ui->lcd08->display(digits[1][7+2]);
+    ui->lcd10->display(digits[2][0+2]);
+    ui->lcd11->display(digits[2][1+2]);
+    ui->lcd12->display(digits[2][2+2]);
+    ui->lcd14->display(digits[2][3+2]);
+    ui->lcd15->display(digits[2][4+2]);
+    ui->lcd16->display(digits[2][5+2]);
+    ui->lcd17->display(digits[2][6+2]);
+    ui->lcd18->display(digits[2][7+2]);
+    ui->lcd21->display(digits[3][0+2]);
+    ui->lcd22->display(digits[3][1+2]);
+    ui->lcd23->display(digits[3][2+2]);
+    ui->lcd24->display(digits[3][3+2]);
+    ui->lcd25->display(digits[3][4+2]);
+    ui->lcd26->display(digits[3][5+2]);
+    ui->lcd27->display(digits[3][6+2]);
+    ui->lcd28->display(digits[3][7+2]);
+    ui->lcd31->display(digits[4][0+2]);
+    ui->lcd32->display(digits[4][1+2]);
+    ui->lcd33->display(digits[4][2+2]);
+    ui->lcd34->display(digits[4][3+2]);
+    ui->lcd35->display(digits[4][4+2]);
+    ui->lcd36->display(digits[4][5+2]);
+    ui->lcd37->display(digits[4][6+2]);
+    ui->lcd38->display(digits[4][7+2]);
 }
 
 if(y=='2'){
-    ui->lcd00_3->display(hardcord[1][0]);
-    ui->lcd01_3->display(hardcord[1][1]);
-    ui->lcd03_3->display(hardcord[1][2]);
-    ui->lcd04_3->display(hardcord[1][3]);
-    ui->lcd05_3->display(hardcord[1][4]);
-    ui->lcd06_3->display(hardcord[1][5]);
-    ui->lcd07_3->display(hardcord[1][6]);
-    ui->lcd08_3->display(hardcord[1][7]);
-    ui->lcd10_3->display(hardcord[2][0]);
-    ui->lcd11_3->display(hardcord[2][1]);
-    ui->lcd12_3->display(hardcord[2][2]);
-    ui->lcd14_3->display(hardcord[2][3]);
-    ui->lcd15_3->display(hardcord[2][4]);
-    ui->lcd16_3->display(hardcord[2][5]);
-    ui->lcd17_3->display(hardcord[2][6]);
-    ui->lcd18_3->display(hardcord[2][7]);
-    ui->lcd21_2->display(hardcord[3][0]);
-    ui->lcd22_3->display(hardcord[3][1]);
-    ui->lcd23_3->display(hardcord[3][2]);
-    ui->lcd24_3->display(hardcord[3][3]);
-    ui->lcd25_3->display(hardcord[3][4]);
-    ui->lcd26_3->display(hardcord[3][5]);
-    ui->lcd27_3->display(hardcord[3][6]);
-    ui->lcd28_3->display(hardcord[3][7]);
-    ui->lcd31_3->display(hardcord[4][0]);
-    ui->lcd32_3->display(hardcord[4][1]);
-    ui->lcd33_3->display(hardcord[4][2]);
-    ui->lcd34_3->display(hardcord[4][3]);
-    ui->lcd35_3->display(hardcord[4][4]);
-    ui->lcd36_3->display(hardcord[4][5]);
-    ui->lcd37_3->display(hardcord[4][6]);
-    ui->lcd38_3->display(hardcord[4][7]);
+    ui->lcd00_3->display(digits[1][0+2]);
+    ui->lcd01_3->display(digits[1][1+2]);
+    ui->lcd03_3->display(digits[1][2+2]);
+    ui->lcd04_3->display(digits[1][3+2]);
+    ui->lcd05_3->display(digits[1][4+2]);
+    ui->lcd06_3->display(digits[1][5+2]);
+    ui->lcd07_3->display(digits[1][6+2]);
+    ui->lcd08_3->display(digits[1][7+2]);
+    ui->lcd10_3->display(digits[2][0+2]);
+    ui->lcd11_3->display(digits[2][1+2]);
+    ui->lcd12_3->display(digits[2][2+2]);
+    ui->lcd14_3->display(digits[2][3+2]);
+    ui->lcd15_3->display(digits[2][4+2]);
+    ui->lcd16_3->display(digits[2][5+2]);
+    ui->lcd17_3->display(digits[2][6+2]);
+    ui->lcd18_3->display(digits[2][7+2]);
+    ui->lcd21_2->display(digits[3][0+2]);
+    ui->lcd22_3->display(digits[3][1+2]);
+    ui->lcd23_3->display(digits[3][2+2]);
+    ui->lcd24_3->display(digits[3][3+2]);
+    ui->lcd25_3->display(digits[3][4+2]);
+    ui->lcd26_3->display(digits[3][5+2]);
+    ui->lcd27_3->display(digits[3][6+2]);
+    ui->lcd28_3->display(digits[3][7+2]);
+    ui->lcd31_3->display(digits[4][0+2]);
+    ui->lcd32_3->display(digits[4][1+2]);
+    ui->lcd33_3->display(digits[4][2+2]);
+    ui->lcd34_3->display(digits[4][3+2]);
+    ui->lcd35_3->display(digits[4][4+2]);
+    ui->lcd36_3->display(digits[4][5+2]);
+    ui->lcd37_3->display(digits[4][6+2]);
+    ui->lcd38_3->display(digits[4][7+2]);
 }
 
 if(y=='3'){
-    ui->lcd00_4->display(hardcord[1][0]);
-    ui->lcd01_4->display(hardcord[1][1]);
-    ui->lcd03_4->display(hardcord[1][2]);
-    ui->lcd04_4->display(hardcord[1][3]);
-    ui->lcd05_4->display(hardcord[1][4]);
-    ui->lcd06_4->display(hardcord[1][5]);
-    ui->lcd07_4->display(hardcord[1][6]);
-    ui->lcd08_4->display(hardcord[1][7]);
-    ui->lcd10_4->display(hardcord[2][0]);
-    ui->lcd11_4->display(hardcord[2][1]);
-    ui->lcd12_4->display(hardcord[2][2]);
-    ui->lcd14_4->display(hardcord[2][3]);
-    ui->lcd15_4->display(hardcord[2][4]);
-    ui->lcd16_4->display(hardcord[2][5]);
-    ui->lcd17_4->display(hardcord[2][6]);
-    ui->lcd18_4->display(hardcord[2][7]);
-    ui->lcd21_3->display(hardcord[3][0]);
-    ui->lcd22_4->display(hardcord[3][1]);
-    ui->lcd23_4->display(hardcord[3][2]);
-    ui->lcd24_4->display(hardcord[3][3]);
-    ui->lcd25_4->display(hardcord[3][4]);
-    ui->lcd26_4->display(hardcord[3][5]);
-    ui->lcd27_4->display(hardcord[3][6]);
-    ui->lcd28_4->display(hardcord[3][7]);
-    ui->lcd31_4->display(hardcord[4][0]);
-    ui->lcd32_4->display(hardcord[4][1]);
-    ui->lcd33_4->display(hardcord[4][2]);
-    ui->lcd34_4->display(hardcord[4][3]);
-    ui->lcd35_4->display(hardcord[4][4]);
-    ui->lcd36_4->display(hardcord[4][5]);
-    ui->lcd37_4->display(hardcord[4][6]);
-    ui->lcd38_4->display(hardcord[4][7]);
+    ui->lcd00_4->display(digits[1][0+2]);
+    ui->lcd01_4->display(digits[1][1+2]);
+    ui->lcd03_4->display(digits[1][2+2]);
+    ui->lcd04_4->display(digits[1][3+2]);
+    ui->lcd05_4->display(digits[1][4+2]);
+    ui->lcd06_4->display(digits[1][5+2]);
+    ui->lcd07_4->display(digits[1][6+2]);
+    ui->lcd08_4->display(digits[1][7+2]);
+    ui->lcd10_4->display(digits[2][0+2]);
+    ui->lcd11_4->display(digits[2][1+2]);
+    ui->lcd12_4->display(digits[2][2+2]);
+    ui->lcd14_4->display(digits[2][3+2]);
+    ui->lcd15_4->display(digits[2][4+2]);
+    ui->lcd16_4->display(digits[2][5+2]);
+    ui->lcd17_4->display(digits[2][6+2]);
+    ui->lcd18_4->display(digits[2][7+2]);
+    ui->lcd21_3->display(digits[3][0+2]);
+    ui->lcd22_4->display(digits[3][1+2]);
+    ui->lcd23_4->display(digits[3][2+2]);
+    ui->lcd24_4->display(digits[3][3+2]);
+    ui->lcd25_4->display(digits[3][4+2]);
+    ui->lcd26_4->display(digits[3][5+2]);
+    ui->lcd27_4->display(digits[3][6+2]);
+    ui->lcd28_4->display(digits[3][7+2]);
+    ui->lcd31_4->display(digits[4][0+2]);
+    ui->lcd32_4->display(digits[4][1+2]);
+    ui->lcd33_4->display(digits[4][2+2]);
+    ui->lcd34_4->display(digits[4][3+2]);
+    ui->lcd35_4->display(digits[4][4+2]);
+    ui->lcd36_4->display(digits[4][5+2]);
+    ui->lcd37_4->display(digits[4][6+2]);
+    ui->lcd38_4->display(digits[4][7+2]);
 }
 
 if(y=='4'){
-    ui->lcd00_5->display(hardcord[1][0]);
-    ui->lcd01_5->display(hardcord[1][1]);
-    ui->lcd03_5->display(hardcord[1][2]);
-    ui->lcd04_5->display(hardcord[1][3]);
-    ui->lcd05_5->display(hardcord[1][4]);
-    ui->lcd06_5->display(hardcord[1][5]);
-    ui->lcd07_5->display(hardcord[1][6]);
-    ui->lcd08_5->display(hardcord[1][7]);
-    ui->lcd10_5->display(hardcord[2][0]);
-    ui->lcd11_5->display(hardcord[2][1]);
-    ui->lcd12_5->display(hardcord[2][2]);
-    ui->lcd14_5->display(hardcord[2][3]);
-    ui->lcd15_5->display(hardcord[2][4]);
-    ui->lcd16_5->display(hardcord[2][5]);
-    ui->lcd17_5->display(hardcord[2][6]);
-    ui->lcd18_5->display(hardcord[2][7]);
-    ui->lcd21_4->display(hardcord[3][0]);
-    ui->lcd22_5->display(hardcord[3][1]);
-    ui->lcd23_5->display(hardcord[3][2]);
-    ui->lcd24_5->display(hardcord[3][3]);
-    ui->lcd25_5->display(hardcord[3][4]);
-    ui->lcd26_5->display(hardcord[3][5]);
-    ui->lcd27_5->display(hardcord[3][6]);
-    ui->lcd28_5->display(hardcord[3][7]);
-    ui->lcd31_5->display(hardcord[4][0]);
-    ui->lcd32_5->display(hardcord[4][1]);
-    ui->lcd33_5->display(hardcord[4][2]);
-    ui->lcd34_5->display(hardcord[4][3]);
-    ui->lcd35_5->display(hardcord[4][4]);
-    ui->lcd36_5->display(hardcord[4][5]);
-    ui->lcd37_5->display(hardcord[4][6]);
-    ui->lcd38_5->display(hardcord[4][7]);
+    ui->lcd00_5->display(digits[1][0+2]);
+    ui->lcd01_5->display(digits[1][1+2]);
+    ui->lcd03_5->display(digits[1][2+2]);
+    ui->lcd04_5->display(digits[1][3+2]);
+    ui->lcd05_5->display(digits[1][4+2]);
+    ui->lcd06_5->display(digits[1][5+2]);
+    ui->lcd07_5->display(digits[1][6+2]);
+    ui->lcd08_5->display(digits[1][7+2]);
+    ui->lcd10_5->display(digits[2][0+2]);
+    ui->lcd11_5->display(digits[2][1+2]);
+    ui->lcd12_5->display(digits[2][2+2]);
+    ui->lcd14_5->display(digits[2][3+2]);
+    ui->lcd15_5->display(digits[2][4+2]);
+    ui->lcd16_5->display(digits[2][5+2]);
+    ui->lcd17_5->display(digits[2][6+2]);
+    ui->lcd18_5->display(digits[2][7+2]);
+    ui->lcd21_4->display(digits[3][0+2]);
+    ui->lcd22_5->display(digits[3][1+2]);
+    ui->lcd23_5->display(digits[3][2+2]);
+    ui->lcd24_5->display(digits[3][3+2]);
+    ui->lcd25_5->display(digits[3][4+2]);
+    ui->lcd26_5->display(digits[3][5+2]);
+    ui->lcd27_5->display(digits[3][6+2]);
+    ui->lcd28_5->display(digits[3][7+2]);
+    ui->lcd31_5->display(digits[4][0+2]);
+    ui->lcd32_5->display(digits[4][1+2]);
+    ui->lcd33_5->display(digits[4][2+2]);
+    ui->lcd34_5->display(digits[4][3+2]);
+    ui->lcd35_5->display(digits[4][4+2]);
+    ui->lcd36_5->display(digits[4][5+2]);
+    ui->lcd37_5->display(digits[4][6+2]);
+    ui->lcd38_5->display(digits[4][7+2]);
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -16,6 +16,7 @@
 extern QByteArray line;
 //extern QList<QByteArray> lines;
 extern int hardcord[5][8];
+extern int digits[5][13];
 extern int max_num;
 extern QString lines;
 
